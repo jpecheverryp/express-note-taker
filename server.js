2e0d1f0396be8234efb9e5ca8a67e3fe51a0fa5f
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2021-03-04 16:13:43 -0500

    added route for api/notes, it return the json database

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app  = express();
 const path = require("path");
+const fs = require("fs");
 
 const PORT = 3000;
 
@@ -11,6 +12,12 @@ app.get("/", (req, res) => {
 })
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname + "/public" + "/notes.html"))
+})
+
+app.get("/api/notes", async (req, res) => {
+    const rawData = fs.readFileSync("./db/db.json")
+    const data = JSON.parse(rawData);
+    res.json(data);
 })
 
 app.listen(PORT, () => {
